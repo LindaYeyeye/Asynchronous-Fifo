896018f191976c8ce6f71400257b3d96ebfa9472
Qingyeyu <66467271+Gienne-608@users.noreply.github.com>
2022-04-13 10:22:47 +0800

    Update fifomem.v

--- fifomem.v
+++ fifomem.v
@@ -1,7 +1,7 @@
 module fifomem
 #(
     parameter  DATASIZE = 32, // Memory data word width               
-    parameter  ADDRSIZE = 6  // ���Ϊ8����ַΪ3λ���ɣ�����ඨ��һλ��ԭ���������ж��ǿջ���������ϸ�ں��Ľ���
+    parameter  ADDRSIZE = 6  // 指针地址位宽设定为6，地址宽度则为5(定义为6是为进位)，fifo深度则为2^5
 ) // Number of mem address bits
 (
     output [DATASIZE-1:0] rdata, 
@@ -11,10 +11,10 @@ module fifomem
 );
 
  // RTL Verilog memory model
-localparam DEPTH = 1<<ADDRSIZE;   // �����൱�ڳ˷���2^4
-reg [DATASIZE-1:0] mem [0:DEPTH-1]; //����2^4��λ��λ8������
+localparam DEPTH = 1<<ADDRSIZE;   // 左移相当于乘法，2^6
+reg [DATASIZE-1:0] mem [0:DEPTH-1]; //fifo深度+位宽定义
 assign rdata = mem[raddr];
-always @(posedge wclk)  //��дʹ����Ч�һ�δд����ʱ������д��洢ʵ���У�ע����������wclkͬ����
+always @(posedge wclk)  //当写使能有效且还未写满的时候将数据写入存储实体中，注意这里是与wclk同步的
     if (wclken && !wfull)
         mem[waddr] <= wdata;
  endmodule
